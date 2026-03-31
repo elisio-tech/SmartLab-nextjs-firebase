69edbbcfc43850752f99c9cc88d286a8c3c66e34
Elisio Augusto <elisiouiux@gmail.com>
2026-03-31 17:44:05 +0100

    zod validations

---
+++ src/app/lib/validations/login.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email({ message: "Email inválido" }),
+  password: z.string().min(6, { message: "Mínimo 6 caracteres" }),
+});
