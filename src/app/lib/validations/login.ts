749c1c28827ebffb75c61c9a09ad7a64affb1030
Elisio Augusto <elisiouiux@gmail.com>
2026-04-01 00:06:28 +0100

    update login

--- src/app/lib/validations/login.ts
+++ src/app/lib/validations/login.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
-export const loginSchema = z.object({
-  email: z.string().email({ message: "Email inválido" }),
-  password: z.string().min(6, { message: "Mínimo 6 caracteres" }),
+
+export const loginShema = z.object({
+  email: z
+    .string()
+    .min(1, "Email é obrigatório")
+    .email("Por favor, insira um e-mail válido."),
+  password: z.string().min(1, "Senha é obrigatória"),
 });
