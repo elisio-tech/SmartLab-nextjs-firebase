2bd17d4b57b2e24182ff4fa4a6ab815c2b45fada
Elisio Augusto <elisiouiux@gmail.com>
2026-04-01 18:27:47 +0100

    get patients

---
+++ src/app/service/pacientesService.ts
@@ -0,0 +1,28 @@
+import { addDoc, collection, getDocs, serverTimestamp } from "firebase/firestore";
+import { db } from "../lib/firebase"; 
+import { Patient } from "../types/patient";
+
+const pacientsRef = collection(db, "patients")
+
+export async function getPatients() {
+    const snapshot = await getDocs(pacientsRef);
+
+    return snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+}
+
+
+
+export async function createPatient(
+  data: Omit<Patient, "id" | "createdAt">
+) {
+  const docRef = await addDoc(pacientsRef, {
+    ...data,
+    createdAt: serverTimestamp(),
+  });
+
+  return { id: docRef.id, ...data };
+}
+
