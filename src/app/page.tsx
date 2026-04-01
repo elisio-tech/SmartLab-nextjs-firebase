78ed487d33e48b25bb336db65e9af575c2d92143
Elisio Augusto <elisiouiux@gmail.com>
2026-04-01 17:58:25 +0100

    update dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+"use client";
+import { useAuth } from "@/app/lib/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default function page() {
-  return <div>Hi, smartlab!</div>;
+export default function Page() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login");
+    }
+  }, [user, loading, router]);
+
+  if (loading) return <>Carregando...</>;
+
+  return <div>Dashboard app</div>;
 }
