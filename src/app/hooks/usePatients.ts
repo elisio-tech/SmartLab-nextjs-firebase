2d0ffeb88ed7266cf5b0158d7f364bb24e941f56
Elisio Augusto <elisiouiux@gmail.com>
2026-04-02 21:02:26 +0100

    fix: getPatients

--- src/app/hooks/usePatients.ts
+++ src/app/hooks/usePatients.ts
@@ -1,19 +1,25 @@
-"use client"
+"use client";
+
 import { useEffect, useState } from "react";
 import { Patient } from "../types/patient";
 import { getPatients } from "../service/pacientesService";
 
 export function usePatients() {
     const [patients, setPatients] = useState<Patient[]>([]);
+    const [loading, setLoading] = useState(true);
 
-    useEffect(()=> {
-        async function load() {
+    async function load() {
+        try {
             const data = await getPatients();
             setPatients(data);
+        } finally {
+            setLoading(false);
         }
+    }
+
+    useEffect(() => {
         load();
     }, []);
 
-    console.log(patients)
-    return patients;
+    return { patients, loading, reload: load };
 }
