a2935caf7af41e8f0dde546f02bb5901e9efc55d
Elisio Augusto <elisiouiux@gmail.com>
2026-04-02 20:03:12 +0100

    update firebase configs

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -6,7 +6,6 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
@@ -16,3 +15,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db =  getFirestore(app)
 export const auth = getAuth(app);
+
+
