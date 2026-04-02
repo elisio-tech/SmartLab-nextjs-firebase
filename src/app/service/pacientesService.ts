b45475651ecb68c123b8964ecc7b99e95dbb280d
Elisio Augusto <elisiouiux@gmail.com>
2026-04-02 23:59:32 +0100

    update

--- src/app/service/pacientesService.ts
+++ src/app/service/pacientesService.ts
@@ -1,21 +1,27 @@
-import { addDoc, collection, deleteDoc, doc, getDocs, serverTimestamp, updateDoc } from "firebase/firestore";
-import { db } from "../lib/firebase"; 
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../lib/firebase";
 import { Patient } from "../types/patient";
 
-const pacientsRef = collection(db, "smartlab-db")
+const pacientsRef = collection(db, "smartlab-db");
 
 export async function getPatients(): Promise<Patient[]> {
-    const snapshot = await getDocs(pacientsRef);
+  const snapshot = await getDocs(pacientsRef);
 
-    return snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...(doc.data() as Omit<Patient, "id">)
-    }));
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as Omit<Patient, "id">),
+  }));
 }
 
-export async function createPatient(
-  data: Omit<Patient, "id" | "createdAt">
-) {
+export async function createPatient(data: Omit<Patient, "id" | "createdAt">) {
   const docRef = await addDoc(pacientsRef, {
     ...data,
     createdAt: serverTimestamp(),
@@ -25,23 +31,19 @@ export async function createPatient(
 }
 
 export async function updatePatient(id: string, data: Partial<Patient>) {
-  const docRef = doc(db, "smartlab-db", id)
   try {
-    await updateDoc(docRef, {
-        ...data,
-        updatedAt: serverTimestamp(), 
-    });
+    const docRef = doc(db, "smartlab-db", id);
+    await updateDoc(docRef, { ...data, updatedAt: serverTimestamp() });
   } catch (error) {
-    console.log("Erro ao atualizar", error);
+    throw new Error(`Erro ao atualizar paciente: ${error}`);
   }
 }
 
-
 export async function deletePatient(id: string) {
-  const docRef = doc(db, "smartlab-db", id);
   try {
+    const docRef = doc(db, "smartlab-db", id);
     await deleteDoc(docRef);
-  } catch (error){
+  } catch (error) {
     console.log("Erro ao deletar", error);
   }
 }
