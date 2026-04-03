5ff1ccfab9671e60f394a035efca795d6126b2b1
Elisio Augusto <elisiouiux@gmail.com>
2026-04-03 14:32:21 +0100

    createRecord done

--- src/app/service/medicalRecord-service.ts
+++ src/app/service/medicalRecord-service.ts
@@ -1,9 +1,11 @@
 import {
+  addDoc,
   collection,
   deleteDoc,
   doc,
   getDoc,
   getDocs,
+  serverTimestamp,
 } from "firebase/firestore";
 import { db } from "../lib/firebase";
 import { MedicalRecord } from "../types/medicalRecord";
@@ -19,8 +21,15 @@ export async function getRecords(): Promise<MedicalRecord[]> {
   }));
 }
 
-
-
+export async function createRecord(
+  data: Omit<MedicalRecord, "id" | "createAt">,
+) {
+  const docRef = await addDoc(recordRef, {
+    ...data,
+    createAt: serverTimestamp(),
+  });
+  return docRef.id;
+}
 
 export async function deleteRecord(recordID: string) {
   const docRef = doc(db, "records", recordID);
