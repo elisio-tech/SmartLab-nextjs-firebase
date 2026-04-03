4cedc3c166ee3b21f787285438aada683b8463c7
Elisio Augusto <elisiouiux@gmail.com>
2026-04-03 02:18:41 +0100

    getById DONE

--- src/app/service/user-service.ts
+++ src/app/service/user-service.ts
@@ -3,6 +3,7 @@ import {
   collection,
   deleteDoc,
   doc,
+  getDoc,
   getDocs,
   serverTimestamp,
   updateDoc,
@@ -28,6 +29,20 @@ export async function createUser(data: Omit<User, "id" | "createdAt">) {
   });
 
   return docRef.id;
+}
+
+export async function getUserByID(uid: string): Promise<User | null> {
+  const docRef = doc(db, "users", uid);
+  const snapshot = await getDoc(docRef);
+  
+  if (!snapshot.exists()) {
+    return null;
+  }
+
+  return {
+    id: snapshot.id,
+    ...(snapshot.data() as Omit<User, "id">),
+  };
 }
 
 export async function updateUser(id: string, data: Partial<User>) {
