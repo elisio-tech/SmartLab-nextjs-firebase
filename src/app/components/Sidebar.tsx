e8847dc42a49c5ed95f26770eb171da5de0630ff
Elisio Augusto <elisiouiux@gmail.com>
2026-04-04 16:02:43 +0100

    remake sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -14,15 +14,19 @@ export default function AppSidebar() {
 
   return (
     <section className="fixed top-0 left-4 bottom-0">
-      <nav className="flex justify-between flex-col h-screen">
+      <nav className="flex justify-between flex-col h-screen border-zinc-200 border-r">
         <div className="">
+          <h1></h1>
           <div className="flex flex-col gap-8">
             <div>
               <h2 className="text-sm uppercase mb-2">Main</h2>
               <div className="flex gap-y-4 flex-col">
                 {navLinks.map((link, i) => (
                   <div key={i}>
-                    <Link href={link.path}>{link.name}</Link>
+                    <div className="flex items-center gap-x-4">
+                      <div>{link.icon}</div>
+                      <Link href={link.path}>{link.name}</Link>
+                    </div>
                     {link.submenu && (
                       <div className="mt-2 ml-4">
                         {link.submenu.map((sublink, i) => (
