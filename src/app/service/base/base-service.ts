f54af6eb5f06c87f57571fb1031c059a9961ef01
Elisio Augusto <elisiouiux@gmail.com>
2026-04-06 11:37:27 +0100

    delete doc done

--- src/app/service/base/base-service.ts
+++ src/app/service/base/base-service.ts
@@ -1,6 +1,11 @@
+import { db } from "@/app/lib/firebase";
 import {
   CollectionReference,
+  deleteDoc,
+  doc,
   DocumentData,
+  DocumentSnapshot,
+  getDocs,
   Timestamp,
 } from "firebase/firestore";
 
@@ -20,6 +25,31 @@ export abstract class BaseService<T extends BaseEntity> {
     const message =
       error instanceof Error ? error.message : "An unexpected error occurred";
 
-    
+    throw new Error(`[Firestore ${operation}] ${message}`);
+  }
+
+  async getAll(): Promise<T[]> {
+    try {
+      const snapshot = await getDocs(this.collectionRef);
+      return snapshot.docs.map(this.mapDocumentToEntity);
+    } catch (error) {
+      return this.handleError(error, "getAll");
+    }
+  }
+
+  async delete(id: string): Promise<void> {
+    try {
+      const docRef = doc(db, this.collectionName, id);
+      await deleteDoc(docRef);
+    } catch (error) {
+      this.handleError(error, "delete");
+    }
+  }
+
+  private mapDocumentToEntity(doc: DocumentSnapshot<DocumentData>): T {
+    return {
+      id: doc.id,
+      ...(doc.data() as Omit<T, "id">),
+    } as T;
   }
 }
