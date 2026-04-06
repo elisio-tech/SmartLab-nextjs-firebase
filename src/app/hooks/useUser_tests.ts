6f95098c9300d960a071a91a6e4a62858fef8dd2
Elisio Augusto <elisiouiux@gmail.com>
2026-04-06 14:04:31 +0100

    use user_test

---
+++ src/app/hooks/useUser_tests.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { User } from "../types/user";
+import { userService } from "../service/tests/user_tests";
+
+export function useUserHooks() {
+  const [user, setUser] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function load() {
+      const data = await userService.getAll();
+      setUser(data);
+    }
+
+    load();
+  }, []);
+
+  return { user };
+}
