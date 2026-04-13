5b5a7f1cf225a57c5a7f00b9acfff46e8623d0e8
Elisio Augusto <elisiouiux@gmail.com>
2026-04-13 21:20:08 +0100

    make login request

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import AuthLayout from "../loyalt";
-import LoginForm from "@/app/components/auth/login-form";
+import LoginForm from "@/components/auth/login-form";
 
 export default function Page() {
   return (
