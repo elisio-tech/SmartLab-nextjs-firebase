b6a7762e6d66e68a8135da8d146b2aa6fded2b8c
Elisio Augusto <elisiouiux@gmail.com>
2026-04-16 21:04:56 +0100

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,48 +1,5 @@
-import Image from "next/image";
 import React from "react";
 
 export default function Header() {
-  return (
-    <header>
-      <nav>
-        <div>
-          <input type="text" placeholder="search" />
-        </div>
-        <div>
-          <div>
-            <button>
-              <Image
-                src={"/icons/settings.png"}
-                alt="notification"
-                width={20}
-                height={18}
-              />
-            </button>
-          </div>
-
-          <div>
-            <button>
-              <Image
-                src={"/icons/notification.png"}
-                alt="notification"
-                width={20}
-                height={18}
-              />
-            </button>
-          </div>
-
-          <div>
-            <button>
-              <Image
-                src={"/users/user-28.jpg"}
-                alt="notification"
-                width={40}
-                height={40}
-              />
-            </button>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+  return <div>Header</div>;
 }
