5acc04fffecc09bc20e2d660bcb69e7d36f2740c
Elisio Augusto <elisiouiux@gmail.com>
2026-04-16 21:05:29 +0100

    start dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,8 +3,8 @@ import Layout from "./layout";
 
 export default function page() {
   return (
-    <div className="ml-75">
-      <h2 className="text-cente">Hi, iam dashboard!</h2>
+    <div className="mt-18 ml-75">
+      <h2 className="">Hi, iam dashboard!</h2>
     </div>
   );
 }
