4ca3a26f1d44d4f6ca97d30006b74844c4b30f61
Elisio Augusto <elisiouiux@gmail.com>
2026-04-16 21:03:52 +0100

    export data

---
+++ src/data/menu.ts
@@ -0,0 +1,58 @@
+export const otherMenu = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: "/icons/plane.png",
+  },
+  {
+    name: "Pacientes",
+    path: "/dashboard/patients",
+    icon: "/icons/calendar.png",
+  },
+  {
+    name: "Exames",
+    path: "/dashboard/exams",
+    icon: "/icons/drop.png",
+  },
+];
+
+export const menu = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: "/icons/grid.png",
+  },
+  {
+    name: "Pacientes",
+    path: "/dashboard/patients",
+    icon: "/icons/shild.png",
+  },
+  {
+    name: "Exames",
+    path: "/dashboard/exams",
+    icon: "/icons/clip.png",
+  },
+  {
+    name: "Consultas",
+    path: "/dashboad/consulting",
+    icon: "/icons/reception.png",
+  },
+  {
+    name: "Relatórios Gerais",
+    path: "/dashboard/records",
+    icon: "/icons/folder_1.png",
+  },
+];
+
+export const footer = [
+  {
+    name: "Help center",
+    path: "/dashboard",
+    icon: "/icons/raio-x.png",
+  },
+  {
+    name: "Settings",
+    path: "/dashboard/patients",
+    icon: "/icons/set.png",
+  },
+];
