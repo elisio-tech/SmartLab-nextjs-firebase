bb5389f0aa632ed976b2e7223e5cb7d67d0b5496
Elisio Augusto <elisiouiux@gmail.com>
2026-04-18 10:47:19 +0100

    change tab

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "./layout";
 import Tab from "@/components/Tab";
+import Metrics from "@/components/Metrics";
 
 export default function page() {
   return (
     <div className="mt-26 ml-24 mx-12">
       <Tab />
+      <Metrics />
     </div>
   );
 }
