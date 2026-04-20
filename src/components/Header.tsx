91b296f465cfd22eaf1847369982bc74bcae7ef0
Elisio Augusto <elisiouiux@gmail.com>
2026-04-20 20:35:43 +0100

    header done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,56 +3,56 @@ import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  const navItems = [
-    "Dashboard",
-    "Patients",
-    "Appointments",
-    "Docs",
-    "Study Materials",
-  ];
-
   return (
-    <header>
-      <div>
-        <div>
-          <Image src={"/logo/logo_02.png"} alt="logo" width={40} height={40} />
-          <span>Symptra</span>
-        </div>
+    <header className="fixed top-0 left-72 right-0 z-50">
+      <nav className="mx-4 px-6 h-20 flex items-center justify-between">
+        <h3>Reports</h3>
 
-        <div>
-          {navItems.map((item) => (
-            <div key={item}>
-              <Link href={`/${item.toLowerCase()}`}>{item}</Link>
-            </div>
-          ))}
-        </div>
-
-        <div>
-          <div>
-            <div>
-              <Image
-                src={"/icons/sett.png"}
-                width={40}
-                height={40}
-                alt="user"
-              />
-            </div>
-          </div>
-          <div>
-            <Image src={"/users/user.jpg"} width={40} height={40} alt="user" />
-            <div>
-              <h3>Robert Fox</h3>
-              <span>Surgean</span>
-            </div>
+        <div className="flex items-center gap-4">
+          <button className="p-2 rounded-lg bg-white">
             <Image
-              src={"/icons/arrowDown.png"}
-              width={40}
-              height={40}
+              src="/icons/nott.png"
+              width={24}
+              height={24}
+              alt="settings"
+            />
+          </button>
+
+          <button className="p-2 rounded-lg bg-white">
+            <Image
+              src="/icons/settt.png"
+              width={24}
+              height={24}
+              alt="settings"
+            />
+          </button>
+
+          <div className="flex items-center gap-3 cursor-pointer">
+            <Image
+              src="/users/user.jpg"
+              width={36}
+              height={36}
+              className="rounded-lg"
               alt="user"
             />
+
+            <div className="flex items-center gap-4">
+              <div className="hidden  leading-tight sm:flex flex-col">
+                <h3 className="text-sm font-medium text-gray-800">
+                  Robert Fox
+                </h3>
+                <span className="text-xs text-gray-500">
+                  robert.fox@gmail.com
+                </span>
+              </div>
+
+              <div className="flex items-center bg-white justify-center w-6 h-6 rounded-sm">
+                <Image src="/icons/arr.png" width={16} height={16} alt="menu" />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 }
