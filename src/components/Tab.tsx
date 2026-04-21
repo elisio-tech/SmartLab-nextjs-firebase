bfbe98de13bc9cb3526e6cd3b150d09e1f5aa4e9
Elisio Augusto <elisiouiux@gmail.com>
2026-04-21 20:15:42 +0100

    ultimate tab

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,5 +1,25 @@
 import React from "react";
-
+import Image from "next/image";
 export default function Tab() {
-  return <div>Tab</div>;
+  return (
+    <div>
+      <div className="flex items-center gap-3">
+        <Image
+          src={"/icons/folder120.png"}
+          alt="folder"
+          width={24}
+          height={24}
+        />
+        <h3>Home Page</h3>
+        <Image src={"/icons/arrow01.png"} alt="folder" width={20} height={20} />
+        <Image
+          src={"/icons/folder120.png"}
+          alt="folder"
+          width={24}
+          height={24}
+        />
+        <h3>Users</h3>
+      </div>
+    </div>
+  );
 }
